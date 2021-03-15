134e0b0a931d158cf4787b84331791f22234ad67
rustymagnet3000 <5367141+rustymagnet3000@users.noreply.github.com>
2021-03-15 06:25:36 +0000

    Followed pytest best practice and split out test folder https://docs.pytest.org/en/stable/goodpractices.html

--- src/app.py
+++ src/app.py
@@ -2,24 +2,10 @@ import requests
 import os
 from json import loads, dumps
 import logging
-import boto3
-import botocore
 
-CONFIG = botocore.config.Config(retries={'max_attempts': 0})
 
 LOGGER = logging.getLogger()
 LOGGER.setLevel(logging.INFO)
-
-
-def get_lambda_client():
-    return boto3.client(
-        'lambda',
-        aws_access_key_id='',
-        aws_secret_access_key='',
-        region_name='eu-west-2',
-        endpoint_url='',
-        config=CONFIG
-    )
 
 
 def get_secret_ingrediant():
@@ -45,11 +31,12 @@ API_PATH = '{0}post'.format(HOSTNAME)
 
 
 def handler(event, context):
-    rec_body = dumps(event)
     LOGGER.info("Handler called!\tevent type:{0}".format(type(event)))
     if "spice" in event:
         recipe_response = send_cake_recipe(event["spice"])
-        return loads(recipe_response.content.decode('utf-8'))
+        recipe_json = loads(recipe_response.content.decode('utf-8'))
+        LOGGER.info("JSON response\n{0}".format(recipe_json))
+        return recipe_json
     else:
         return {
             "statusCode": 200,
--- tests/test_cake.py
+++ tests/test_cake.py
@@ -1,11 +1,14 @@
 import json
 from requests.models import Response
+import boto3
+from moto import mock_lambda
+import src
 
-import app
+# https://docs.pytest.org/en/stable/goodpractices.html
 
 
 def invoke_function_and_get_message(function_name):
-    lambda_client = app.get_lambda_client()
+    lambda_client = boto3.client('lambda', region_name='eu-west-1')
     response = lambda_client.invoke(
         FunctionName=function_name,
         InvocationType='RequestResponse'
@@ -41,7 +44,14 @@ def test_get_environment_variable():
     assert app.get_secret_ingrediant() in 'chocolate'
 
 
+@mock_lambda
 def test_that_lambda_returns_correct_message():
-    payload = invoke_function_and_get_message('lambda')
+    payload = {"args": {}, "data": "", "files": {},
+               "form": {"ingrediant_1": "sugar", "ingrediant_2": "butter", "spice": "nutmeg"},
+               "headers": {"Accept": "*/*", "Accept-Encoding": "gzip, deflate", "Content-Length": "51",
+                           "Content-Type": "application/x-www-form-urlencoded", "Host": "httpbin.org",
+                           "User-Agent": "python-requests/2.25.1",
+                           "X-Amzn-Trace-Id": "Root=1-604ef622-1e2b8d1c300ddd233ab26674"}, "json": "",
+               "origin": "92.234.25.98", "url": "https://httpbin.org/post"}
+    print(payload)
     assert 'Bad recipe' in payload
-
